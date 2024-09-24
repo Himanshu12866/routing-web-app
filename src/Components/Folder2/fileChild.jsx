3de5789bf5f081b309fb5680f3ffb8463fad2d07
Himanshu12866 <manshu010m@gmail.com>
2024-09-24 15:45:12 +0530

    "Added express and mongodb dependencies, updated fileChild.jsx component with minor styling changes and removed rating information"

--- src/Components/Folder2/fileChild.jsx
+++ src/Components/Folder2/fileChild.jsx
@@ -21,21 +21,20 @@ export default function ChildProducts() {
             .then(response => {
                 setProduct(response.data)
             })
-    }, [product , params.id])
+    }, [product, params.id])
     return (
         <div className="detailsBox" style={{ textAlign: "left" }}>
             <div className="row" style={{ height: "100vh" }}>
                 <div className="col-lg-6 col-sm-12">
-                    <img alt="" src={product.image} style={{ height: "500px", width: "100%" }}></img>
+                    <img alt="" src={product.image} style={{ height: "500px", width: "100%", borderRadius: "20px" }}></img>
                 </div>
                 <div className="col-lg-6 col-sm-12">
                     <div style={{ textAlign: "left" }}>
                         <h3 className="text-light">{product.title}</h3>
                         <h4 className="text-light"> <span className="fs-4">₹</span>{product.price} .00</h4>
                     </div>
-                    <p className="text-light"> 
-                        <span className="badge bi bi-star-fill bg-primary">&nbsp;{product.rating.rate}</span> Ratings  
-                        <span className="badge bg-success bi bi-people-fill">{product.rating.count}</span> 
+                    <p className="text-light">
+                        <i> {product.description}</i>
                     </p>
 
 
