1fd2d783c721c67dded959fe32004772043e08e7
Himanshu12866 <manshu010m@gmail.com>
2024-09-24 16:35:04 +0530

    Modified and created own api successfully

---
+++ src/API/api.js
@@ -0,0 +1,29 @@
+
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+const app = express()
+
+app.get("/" , (req ,res) => {
+    res.send("<h1>Welcome to the Fashion Home</h1>")
+    res.end()
+});
+app.get("/men" , (req,res) => {
+    res.send("<h1>Men's Fashion</h1>")
+    res.end()
+})
+
+app.get("/products" , (req,res) => {
+    MongoClient.connect(url).then(
+        clientObj => {
+            const db = clientObj.db("admin")
+            db.collection("products").find({}).toArray().then(document => {
+                res.send(document)
+                res.end()
+            })
+        }
+    )
+})
+app.listen(5500)
+console.log("Server Started at http://127.0.0.1:5500")
