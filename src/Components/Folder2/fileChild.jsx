dc8645b40b6094471d5962b8bfd87ebfc04d8034
Himanshu12866 <manshu010m@gmail.com>
2024-09-24 16:05:40 +0530

    "Added Link import from react-router-dom, modified state initialization, and updated JSX elements in ChildProducts component"

--- src/Components/Folder2/fileChild.jsx
+++ src/Components/Folder2/fileChild.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -13,7 +13,8 @@ export default function ChildProducts() {
         rating: {
             rate: 0,
             count: 0
-        }
+        },
+        category: ""
     }])
     let params = useParams()
     useEffect(() => {
@@ -28,16 +29,31 @@ export default function ChildProducts() {
                 <div className="col-lg-6 col-sm-12">
                     <img alt="" src={product.image} style={{ height: "500px", width: "100%", borderRadius: "20px" }}></img>
                 </div>
-                <div className="col-lg-6 col-sm-12">
+                <div className="col-lg-6 col-sm-12" >
                     <div style={{ textAlign: "left" }}>
                         <h3 className="text-light">{product.title}</h3>
                         <h4 className="text-light"> <span className="fs-4">₹</span>{product.price} .00</h4>
                     </div>
                     <p className="text-light">
+                        <span className="bi bi-tag-fill text-success ">
+
+                        </span> &nbsp;
+                        {/* {product.category.toUpperCase()} */}
+                    </p>
+                    <p className="text-light">
                         <i> {product.description}</i>
                     </p>
-
-
+                    <div className="row" style={{ marginTop: "22%" }}>
+                        <div className="col-4">
+                            <button className="btn btn-danger w-100 p-3">Add To Cart</button>
+                        </div>
+                        <div className="col-4">
+                            <button className="btn btn-success w-100 p-3">Buy Now</button>
+                        </div>
+                        <div className="col-4">
+                            <Link to={`products`}  className="btn btn-warning w-100 p-3">Go Back To Products</Link>
+                        </div>
+                    </div>
                 </div>
 
             </div>
